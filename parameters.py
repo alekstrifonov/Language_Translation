770adbc8030485f44ca8f5a1d72372767a2ff3d7
alekstrifonov <alekstrifonov64@gmail.com>
2026-02-26 12:33:02 +0200

    Initial training on GPU, try LoRA next

--- parameters.py
+++ parameters.py
@@ -9,20 +9,20 @@ corpusFileName = 'corpusData'
 wordsFileName = 'wordsData'
 modelFileName = 'NMTmodel'
 
-#device = torch.device("cuda:0")
-device = torch.device("cpu")
+device = torch.device("cuda:0")
+# device = torch.device("cpu")
 
 vocab_size = 16000
-layers=2
-embed_size=32
-hidden_size=128
-n_head=2
-dropout=0.1
+layers = 6
+embed_size = 128
+hidden_size = 512
+n_head = 8
+dropout = 0.1
 
 learning_rate = 0.001
-batchSize = 2
+batchSize = 32
 clip_grad = 5.0
 
 maxEpochs = 1
 log_every = 10
-test_every = 2000
+test_every = 200
